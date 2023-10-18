c50194dbf3ee14d11a842e86d4619ba3452b7c15
Timewound <rubenjms1@gmail.com>
2023-10-18 09:43:09 -0300

    seperando html utilizando tags django para estrutura dinamica

--- projeto/urls.py
+++ projeto/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recipes/', include('recipes.urls'))
+    path('', include('recipes.urls'))
     
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,11 @@ from django.http import HttpResponse
 
 
 def home(request):
-    context = {'name': 'eoq mlk'}
+    context = {'name': 'joao'}
     return render(request, 'recipes/pages/home.html', context)
+
+
+def recipes(request, id):
+    context = {'name': 'joao'}
+    return render(request, 'recipes/pages/recipe-view.html', context)
 

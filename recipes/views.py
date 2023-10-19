2ac449ff2adbd23618011e9a4beef9b359cdd398
Timewound <rubenjms1@gmail.com>
2023-10-19 10:20:38 -0300

    preenchendo dados com faker

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 
 
 def home(request):
-    context = {'name': 'joao'}
+    context = {'recipes': [make_recipe() for _ in range(10)]}
     return render(request, 'recipes/pages/home.html', context)
 
 
 def recipes(request, id):
-    context = {'name': 'joao'}
+    context = {'recipe': make_recipe(),
+               'is_detail_page': True}
     return render(request, 'recipes/pages/recipe-view.html', context)
 

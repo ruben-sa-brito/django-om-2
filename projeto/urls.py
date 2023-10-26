23855fc6d11d8c1d6b87485922510d490111271c
Timewound <rubenjms1@gmail.com>
2023-10-26 11:24:08 -0300

    incluindo recipes no admin e diretorio do MEDIA_ROOT

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -24,3 +26,5 @@ urlpatterns = [
     path('', include('recipes.urls'))
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+    
+admin.site.register(Category, CategoryAdmin)    

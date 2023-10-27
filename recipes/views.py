a1c955139ebaaea73a7720da473acd36feab570d
Timewound <rubenjms1@gmail.com>
2023-10-27 10:57:52 -0300

    adiconando logica para publicar apenas receitas is_published, e separando page de categorias

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 
 def home(request):
-    context = {'recipes': [make_recipe() for _ in range(10)]}
-    return render(request, 'recipes/pages/home.html', context)
+    
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    
+    
+    return render(request, 'recipes/pages/home.html', context = {'recipes':recipes})
 
 
 def recipes(request, id):
     context = {'recipe': make_recipe(),
                'is_detail_page': True}
     return render(request, 'recipes/pages/recipe-view.html', context)
+
+
+def category(request, category_id):
+    
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+        ).order_by('-id')
+    
+    
+    return render(request, 'recipes/pages/category.html', context = {'recipes':recipes})
 
